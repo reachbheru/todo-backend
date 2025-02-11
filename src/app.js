9f9dab68243fd08ca52330405c45fa5eea00abcc
reachbheru <141500915+reachbheru@users.noreply.github.com>
2025-02-11 17:31:11 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -4,14 +4,6 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-// configurations
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}));
-app.use(express.json({limit: "32kb"}));
-app.use(express.urlencoded({limit: "32kb", extended: true}));
-app.use(cookieParser());
 
 //import routers
 import usersRouter from "./routers/users.router.js"
