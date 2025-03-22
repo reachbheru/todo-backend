66230c9e1bc3010acb15f650f9a210fa36190b3c
reachbheru <141500915+reachbheru@users.noreply.github.com>
2025-03-22 16:08:26 +0530

    changes

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,33 +5,31 @@ import { User } from "../models/users.model.js";
 import { CognitoJwtVerifier } from "aws-jwt-verify";
 
 const decodeCognitoToken = asyncHandler(async (req, _, next) => {
+  const idToken = req.header("x-id-token");
 
-    const idToken = req.header("x-id-token");
+  if (!idToken) {
+    throw new ApiError(401, "Unauthorized request");
+  }
 
-    if(!idToken){
-        throw new ApiError(401, "Unauthorized request");
+  try {
+    const verifier = CognitoJwtVerifier.create({
+      userPoolId: process.env.COGNITO_USER_POOL_ID,
+      tokenUse: "id",
+      clientId: process.env.COGNITO_CLIENT_ID,
+    });
+
+    const payload = await verifier.verify(idToken);
+    const user = await User.findOne({ email: payload.email }).select(
+      "-password -refreshToken"
+    );
+    if (!user) {
+      throw new ApiError(401, "invalid token");
     }
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, "invalid token");
+  }
+});
 
-    try {
-
-        const verifier = CognitoJwtVerifier.create({
-            userPoolId: process.env.COGNITO_USER_POOL_ID,
-            tokenUse: "id",
-            clientId: process.env.COGNITO_CLIENT_ID,
-        });
-
-        const payload = await verifier.verify(idToken);
-        const user = await User.findOne({email: payload.email}).select("-password -refreshToken");
-        if(!user){
-            throw new ApiError(401, "invalid token");
-        }
-        req.user = user;
-        next();
-
-    } 
-    catch (error) {
-        throw new ApiError(401, "invalid token");
-    }
-})
-
-export { decodeCognitoToken }
+export { decodeCognitoToken };
